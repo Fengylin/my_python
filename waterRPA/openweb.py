09da5bb82975a840d1a9a33741d2d5b4d96944c3
Fengylin <fx.hyy@qq.com>
2022-03-17 21:36:00 +0800

    可以打开浏览器和特定的网站了

---
+++ waterRPA/openweb.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Filename: openweb.py
+
+import os
+import webbrowser
+
+#打开浏览器CHROME
+# 这个太笨了
+# os.startfile("C:\\Users\\何博\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe")
+#这个简单
+webbrowser.open('https://pt.btschool.club/login.php', new=0, autoraise=True)
+
+webbrowser.open('https://www.haidan.video/index.php', new=0, autoraise=True)
